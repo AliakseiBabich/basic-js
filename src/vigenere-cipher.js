e26ce60c8fd7eae7037f735081ffb1aa4f88d4cd
Aliaksei Babich <lex.gmx@gmail.com>
2020-10-31 22:48:38 +0300

    docs: add cipher code

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,11 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    
-  }    
-  decrypt() {
-    
+  constructor(reversed = true){
+    this.reversed = reversed;
+  }
+
+  encrypt(str, key) {
+    key = key.toUpperCase();
+    key = key.padEnd(str.length, key);
+    let space = 0;
+    let answer = str.toUpperCase().split("").map((x,index)=>x.match(/\W|_|[0-9]/g)?((x==" "?(space++,x):x)):(String.fromCharCode((x.charCodeAt()+key[index-space].charCodeAt())%26+65)));
+	  return this.reversed?answer.join(""):answer.reverse().join("");
+  }
+
+  decrypt(str, key) {
+    key = key.toUpperCase();
+    key = key.padEnd(str.length, key);
+    let space = 0;
+    let answer = str.toUpperCase().split("").map((x,index)=>x.match(/\W|_|[0-9]/g)?((x==" "?(space++,x):x)):(String.fromCharCode((x.charCodeAt()-key[index-space].charCodeAt()+26)%26+65)));
+	  return this.reversed?answer.join(""):answer.reverse().join("");
   }
 }
 
